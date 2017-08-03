257ae50f76fe7a9eb6dc6cb68cb28e25c1225487
maimingliang <maimingliang8@163.com>
2017-08-03 23:34:18 +0800

    libx264 ffmpegrun

---
+++ app/src/main/java/com/maiml/ffmpegdemo2/MainActivity.java
@@ -0,0 +1,43 @@
+package com.maiml.ffmpegdemo2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,AvcodecTxtActivity.class));
+            }
+        });
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ExecuteCommandActivity.class));
+            }
+        });
+
+
+        findViewById(R.id.btn3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,VideoPlayActivity.class));
+            }
+        });
+
+    }
+
+
+
+
+}
---
+++ ffmpeglibrary/src/main/java/com/maiml/ffmpeglibrary/FFmpeg.java
@@ -0,0 +1,52 @@
+package com.maiml.ffmpeglibrary;
+
+/**
+ * Created by maimingliang on 2017/7/18.
+ */
+
+public class FFmpeg {
+
+
+    private static FFmpeg sInstance ;
+
+    public static FFmpeg getsInstance(){
+        if(sInstance == null) {
+            sInstance = new FFmpeg();
+        }
+        return sInstance;
+    }
+
+    private FFmpeg(){}
+
+
+    public String getValue(){
+        return avcodecinfo();
+    }
+
+    /**
+     * 执行ffmpeg命令
+     * @param commands
+     * @return
+     */
+    public int executeCommand(String[] commands){
+        return execute(commands);
+    }
+
+
+
+    public native int play(Object surface,String path);
+    private native String avcodecinfo();
+    private native int execute(String[] commands);
+
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("avutil-55");
+        System.loadLibrary("swresample-2");
+        System.loadLibrary("avcodec-57");
+        System.loadLibrary("avfilter-6");
+        System.loadLibrary("swscale-4");
+        System.loadLibrary("avformat-57");
+        System.loadLibrary("avresample-3");
+    }
+
+}
